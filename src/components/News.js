5fda2f65a1d2878bfe8ca5f3a6a1c50183d95ecf
Muaaz <muaazzahida@gmail.com>
2024-01-16 21:14:40 +0530

    Made the code concise by making next and prev clicks as one along with componentdidMount

--- src/components/News.js
+++ src/components/News.js
@@ -28,51 +28,39 @@ export class News extends Component {
     };
   }
 
-  async componentDidMount() {
-    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=90bc1dd6e4004ca59635148d8d820d09&page=1&pageSize=${this.props.pageSize}`;
+  updateNews = async()=>{
+    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=90bc1dd6e4004ca59635148d8d820d09&page=${this.state.page}&pageSize=${this.props.pageSize}`;
     this.setState({loading : true})
     let data = await fetch(url);
     let parsedData = await data.json();
-
     this.setState({
       articles: parsedData.articles,
       totalResults: parsedData.totalResults,
       loading : false
     });
+  };
+
+  async componentDidMount() {
+    this.updateNews();
+  }
+
+  handlePrevClick = async () => {
+    
+    this.setState({
+      page: this.state.page - 1,
+    });
+    this.updateNews();
+  }
+
+    handleNextClick = async () => {
+      this.setState({
+        page: this.state.page + 1
+      });
+
+      this.updateNews();
   }
 
   render() {
-    let handlePrevClick = async () => {
-
-      let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=90bc1dd6e4004ca59635148d8d820d09&page=${
-        this.state.page - 1
-      }&pageSize=${this.props.pageSize}`;
-      this.setState({loading : true})
-      let data = await fetch(url);
-      let parsedData = await data.json();
-      this.setState({
-        page: this.state.page - 1,
-        articles: parsedData.articles,
-        loading : false
-      });
-    };
-
-    let handleNextClick = async () => {
-
-      if(!(this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize))){
-          let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=90bc1dd6e4004ca59635148d8d820d09&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
-          this.setState({loading : true})
-          let data = await fetch(url);
-          let parsedData = await data.json();
-          this.setState({
-            page: this.state.page + 1,
-            articles: parsedData.articles,
-            loading : false
-          });
-
-        }
-    };
-
     return (
       <div>
         <div className="container my-3">
@@ -108,7 +96,7 @@ export class News extends Component {
             type="button"
             disabled={this.state.page <= 1}
             className="btn btn-dark mb-3"
-            onClick={handlePrevClick}
+            onClick={this.handlePrevClick}
           >
             &larr; Prev
           </button>
@@ -119,7 +107,7 @@ export class News extends Component {
               Math.ceil(this.state.totalResults / this.props.pageSize)
             }
             className="btn btn-dark mb-3"
-            onClick={handleNextClick}
+            onClick={this.handleNextClick}
           >
             Next &rarr;
           </button>
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -5,16 +5,16 @@ export default class NewsItem extends Component {
     let { title, description, imageUrl, newsUrl, author, date, source } = this.props;
     return (
       <div className="card">
-        <span class="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style = {{zIndex:'1', left:'90%'}}>
+        <span className="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style = {{zIndex:'1', left:'90%'}}>
           {source}
-          <span class="visually-hidden">unread messages</span>
+          <span className="visually-hidden">unread messages</span>
         </span>
         <img src={imageUrl} className="card-img-top" alt="..." />
         <div className="card-body">
           <h5 className="card-title">{title}...</h5>
           <p className="card-text">{description}...</p>
           <p className="my-3 card-text">
-            <small class="text-muted">
+            <small className="text-muted">
               By {author} on {date}
             </small>
           </p>
